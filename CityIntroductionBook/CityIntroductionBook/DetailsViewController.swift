94211c446eed47fa5b18e42b59260de639dc5a18
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-04-22 21:21:21 +0300

    tableView

---
+++ CityIntroductionBook/CityIntroductionBook/DetailsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsViewController.swift
+//  CityIntroductionBook
+//
+//  Created by Serdar Altındaş on 22.04.2023.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var cityBolgeLabel: UILabel!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
--- CityIntroductionBook/CityIntroductionBook/ViewController.swift
+++ CityIntroductionBook/CityIntroductionBook/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "test"
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetailsVC", sender: self)
+    }
 }
 
